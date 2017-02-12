91f80b937e627962a57dd829e84a59b586e0347f
Simon Vermeersch <simonvermeersch@gmail.com>
2017-02-12 16:14:25 +0100

    Helper method to add text to the various panes

--- evennia/web/webclient/static/webclient/js/webclient_gui.js
+++ evennia/web/webclient/static/webclient/js/webclient_gui.js
@@ -253,24 +253,29 @@ function doWindowResize() {
 function onText(args, kwargs) {
     // append message to previous ones, then scroll so latest is at
     // the bottom. Send 'cls' kwarg to modify the output class.
-    var renderto = "main";
+    var renderto = "mainpane";
     if (kwargs["type"] == "help") {
         if (("helppopup" in options) && (options["helppopup"])) {
             renderto = "#helpdialog";
         }
     }
 
-    if (renderto == "main") {
-        var mwin = $("#messagewindow");
-        var cls = kwargs == null ? 'out' : kwargs['cls'];
-        mwin.append("<div class='" + cls + "'>" + args[0] + "</div>");
-        mwin.parent().animate({
-            scrollTop: document.getElementById("messagewindow").scrollHeight
-        }, 0);
-        onNewLine(args[0], null);
+    if (renderto == "mainpane") {
+        addTextToPane(args[0], renderto, kwargs);
     } else {
         openPopup(renderto, args[0]);
     }
+}
+
+function addTextToPane(text, panename, kwargs) {
+    var mwin = $("div[data-name='"+panename+"'").find(".messagewindow");
+    var cls = kwargs == null ? 'out' : kwargs['cls'];
+    mwin.append("<div class='" + cls + "'>" + text + "</div>");
+    mwin.parent().animate({
+        scrollTop: mwin[0].scrollHeight
+    }, 0);
+    onNewLine(args[0], null);
+
 }
 
 // Handle prompt output from the server
@@ -462,7 +467,7 @@ function splitPane(parentpane, direction) {
         var orightml = parentpane.html();
         var cls = direction + "pane";
         var splithtml = $("<div class='" + cls + "' data-size='50'></div>" +
-                          "<div class='" + cls + "' data-size='50'>" + $("#dropdowntemplate").html() + "</div>");
+                          "<div class='" + cls + "' data-size='50'>" + $("#dropdowntemplate").html() + '<div class="messagewindow" role="log"></div></div>');
         parentpane.html(splithtml);
         
         createSplit([splithtml[0], splithtml[1]], direction, [50, 50]);
@@ -552,6 +557,7 @@ function loadPanes(panes) {
             //If no such pane exists, then this is a new pane: add the split buttons.
             if (parentdiv.is(':empty')) {
                 parentdiv.append($("#dropdowntemplate").html());
+                parentdiv.append('<div class="messagewindow" role="log"></div>');
             };
         }
     }
